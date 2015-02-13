5120d461838d74bb78440bb6151c4c889c4b0a85
mskoenz <mskoenz@gmx.net>
2015-02-13 10:00:10 +0100

    43.Commit:	added pyplot.py and autocomlete file mskoenz
    DONE:
    - bash completion file
    - pyplot.py

---
+++ plot/external/pyplot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 
+# Author:  Mario S. Könz <mskoenz@gmx.net>
+# Date:    10.02.2015 10:02:55 CET
+# File:    pyplot.py
+
+import sys
+from addon import *
+
+if __name__ == "__main__":
+    parameter.read(sys.argv)
+    plot(parameter)
